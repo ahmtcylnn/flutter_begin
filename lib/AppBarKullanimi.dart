4c5b16e1340620ee0a6c0a3d6e614b3070f30cab
ahmtcylnn <a.h.c.e@hotmail.com>
2023-01-22 23:56:57 +0300

    appBarPopUpMenu

--- lib/AppBarKullanimi.dart
+++ lib/AppBarKullanimi.dart
@@ -7,50 +7,69 @@ class AppBarKullanimi extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Başlık",
-              style: TextStyle(color: Colors.black, fontSize: 16),
-            ),
-            const Text("Alt Başlık",
-                style: TextStyle(color: Colors.white, fontSize: 13))
-          ],
-        ),
-        centerTitle: false,
-        leading: IconButton(
-          tooltip: "Menu Icon",
-          icon: const Icon(Icons.dehaze),
-          onPressed: () {
-            print("Menü Icon Tıklandı.");
-          },
-        ),
-        actions: [
-          ElevatedButton(
-              onPressed: () {
-                print("Çıkış Tıklandı");
-              },
-              child: const Text(
-                "Çıkış",
-                style: TextStyle(color: Colors.white),
-              )),
-          IconButton(
-            tooltip: "Bilgi",
-            icon: const Icon(Icons.info_outline),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Başlık",
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              const Text("Alt Başlık",
+                  style: TextStyle(color: Colors.white, fontSize: 13))
+            ],
+          ),
+          centerTitle: false,
+          leading: IconButton(
+            tooltip: "Menu Icon",
+            icon: const Icon(Icons.dehaze),
             onPressed: () {
-              print("Bilgi Tıklandı.");
+              print("Menü Icon Tıklandı.");
             },
           ),
-          IconButton(
-            tooltip: "Popup Menu",
-            icon: const Icon(Icons.more_vert),
-            onPressed: (() {
-              print("Popup Menü Tıklandı.");
-            }),
-          )
-        ],
-      ),
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  print("Çıkış Tıklandı");
+                },
+                child: const Text(
+                  "Çıkış",
+                  style: TextStyle(color: Colors.white),
+                )),
+            IconButton(
+              tooltip: "Bilgi",
+              icon: const Icon(Icons.info_outline),
+              onPressed: () {
+                print("Bilgi Tıklandı.");
+              },
+            ),
+            PopupMenuButton(
+              child: Icon(Icons.more_vert),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: const Text(
+                    "Sil",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  value: 1,
+                ),
+                PopupMenuItem(
+                  child: const Text(
+                    "Güncelle",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  value: 2,
+                )
+              ],
+              onSelected: (menuItemValue) {
+                if (menuItemValue == 1) {
+                  print("Sil tıklandı.");
+                }
+                if (menuItemValue == 2) {
+                  print("Güncelle tıklandı.");
+                }
+              },
+            )
+          ]),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
